4c632b6b15775b8d192920ece72797e3a32414ee
farellfebriano <farellfebriano8@gmail.com>
2025-02-10 12:31:07 -0600

    uninstall

---
+++ custom_post/uninstall.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ *
+ *  wordpress will triger this file on plugins uninstall
+ *
+ * @package LalalandPlugin
+ *
+ */
+
+ // make sure that WP_UNINSTALL_PLUGINS is init before initialization
+
+
+if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
+	die;
+}
+
+// Clear Database stored data
+$books = get_posts( array( 'post_type' => 'book', 'numberposts' => -1 ) );
+
+foreach( $books as $book ) {
+	wp_delete_post( $book->ID, true );
+}
+
+
+// Access database via sql
+// global $wpdb;
+
+// $wpdb->query("DELETE FROM wp_posts WHERE post_type ='book'");
